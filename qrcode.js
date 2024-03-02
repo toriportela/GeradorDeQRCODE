6d1b5529c72ee5098e5bf410b72e414941c192df
Vitória Portela <135430898+toriportela@users.noreply.github.com>
2024-03-02 12:42:52 -0300

    Update qrcode.js
    cria a folder qrcodes automaticamente

--- qrcode.js
+++ qrcode.js
@@ -14,13 +14,16 @@ fs.readFile('links.txt', 'utf8', (err, data) => {
     lines.forEach((line, index) => {
         const [link, fileName] = line.split(' - ');
 
-
         const folderName = 'QRCODES';
 
+        // verifica se a pasta já existe se não, a cria
+        if (!fs.existsSync(folderName)) {
+            fs.mkdirSync(folderName);
+        }
 
         const filePath = path.join(folderName, `${fileName.trim()}.png`);
 
-        // especifica a width, height e margin
+        // especifica a largura, altura e margem do QR code
         qr.toFile(filePath, link.trim(), {
             width: 400,
             height: 400,
